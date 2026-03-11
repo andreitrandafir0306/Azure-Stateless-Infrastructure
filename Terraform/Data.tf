95f5ed1c45c1999b8d203d0d80bf2edd07a7ef7a
andreitrandafir0306 <andre.trandafir0306@gmail.com>
2026-03-11 15:26:40 +0200

    Edit Data.tf

--- Terraform/Data.tf
+++ Terraform/Data.tf
@@ -2,7 +2,7 @@
 
 data "azurerm_public_ip" "this" {
   name                = azurerm_public_ip.this.name
-  resource_group_name = azurerm_resource_group.rg.name
+  resource_group_name = data.azurerm_resource_group.rg.name
 }
 
 data "azurerm_resource_group" "rg" {
